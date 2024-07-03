401feafb8b06578f4abd6e3f7be304d92731b8f7
Geter C Cerqueira <106839529+getertel@users.noreply.github.com>
2024-07-03 22:15:44 +0000

    experiência de endpoint e confirmação de commit em estudo meu

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.get('/', function (req, res) {
 app.get('/oi', function (req, res) {
   res.send('Olá, mundo!')
 })
+
+//endpoint experiência minha
+
+app.get('/bug', function (req, res) {
+  res.send('Bugado')
+})
+
  //Lista de personagens
  const lista = ['Rich Sanches', 'Morty Smith', 'Summer Smith']
  //read all - [get] / item
