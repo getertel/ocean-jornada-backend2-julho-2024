69adeb3336a79415368b7dc579d2ce52fa39c1e9
Geter C Cerqueira <106839529+getertel@users.noreply.github.com>
2024-07-03 13:48:23 +0000

    post get

--- index.js
+++ index.js
@@ -5,5 +5,22 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000)
+// Desafio: criar endpoint /oi que exibe "Olá, mundo!"
+app.get('/oi', function (req, res) {
+  res.send('Olá, mundo!')
+})
+ //Lista de personagens
+ const lista = ['Rich Sanches', 'Morty Smith', 'Summer Smith']
+ //read all - [get] / item
+ app.get('/item', function (req, res) {res.send(lista)
+})
+//sinalizar para o express 
 
+app.use(express.json())
+
+//creat - post
+app.post('/item', function (req, res) {
+  console.log(req.body)
+  res.send('Create')
+  })
+app.listen(3000)
